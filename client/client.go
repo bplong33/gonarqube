56f0d2f1f5d0d46d0478a76c116cb380a2f6fda2
Brandon Long <35179860+bplong33@users.noreply.github.com>
2025-05-12 08:08:43 -0400

    Create reusable ResolveGetRequest func

--- client/client.go
+++ client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -19,4 +21,32 @@ func NewClient(baseURL string, token string, httpClient *http.Client) *Client {
 		Token:   token,
 		Client:  httpClient,
 	}
+}
+
+func ResolveGetRequest(
+	uri string,
+	header map[string]string,
+	client *http.Client,
+) []byte {
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		log.Panicln("Error while creating request:", err)
+	}
+	for key, val := range header {
+		req.Header.Add(key, val)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panicln("Failed to execute request:", err)
+	}
+	defer resp.Body.Close()
+
+	// read response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln("Failed to read body:", err)
+	}
+
+	return body
 }
--- services/project.go
+++ services/project.go
@@ -3,9 +3,7 @@ package services
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/bplong33/gonarqube/client"
@@ -38,19 +36,6 @@ type Project struct {
 	Managed          bool   `json:"managed"`
 }
 
-// type ProjectList struct {
-// 	Projects []Project
-// }
-
-// type ProjectSearchOpts struct {
-// 	AnalyzedBefore    string `json:"analyzedBefore"`
-// 	OnProvisionedOnly bool   `json:"onProvisionedOnly"`
-// 	Page              int    `json:"p"`
-// 	Projects          string `json:"projects"`
-// 	PageSize          int    `json:"ps"`
-// 	Query             string `json:"q"`
-// }
-
 func NewProjectClient() *ProjectClient {
 	return &ProjectClient{
 		Client: client.NewClient(
@@ -61,40 +46,25 @@ func NewProjectClient() *ProjectClient {
 	}
 }
 
-func (p *ProjectClient) GetProjects(opts map[string]string) []Project {
-	// TODO: this currently assumes all opts are valid
+func (p *ProjectClient) GetProjects(params map[string]string) []Project {
 	const pageSize int = 200
 	page := 1
 	projects := []Project{}
 
 	// add query params
 	endpoint := p.Client.BaseURL + "/projects/search?"
-	for key, val := range opts {
+	// TODO: this currently assumes all opts are valid
+	for key, val := range params {
 		endpoint = fmt.Sprintf("%s%s=%s&", endpoint, key, val)
 	}
 
 	// token
 	bearer := "Bearer " + p.Client.Token
+	header := map[string]string{"Authorization": bearer}
 
 	for { // while there are more pages
 		uri := fmt.Sprintf("%sp=%d", endpoint, page)
-		req, err := http.NewRequest("GET", uri, nil)
-		if err != nil {
-			log.Panicln("Error while creating request:", err)
-		}
-		req.Header.Add("Authorization", bearer)
-
-		resp, err := p.Client.Client.Do(req)
-		if err != nil {
-			log.Panicln("Failed to execute request:", err)
-		}
-		defer resp.Body.Close()
-
-		// read response
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Panicln("Failed to read body:", err)
-		}
+		body := client.ResolveGetRequest(uri, header, p.Client.Client)
 
 		data := &ProjectResponse{}
 		if err := json.Unmarshal([]byte(body), data); err != nil {
